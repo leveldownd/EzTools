26f16736a17ead8df41b0f643a7e85841dccbbc2
geenva <geenva@users.noreply.github.com>
2021-04-12 21:53:31 +0800

    feat: beta page boiler

---
+++ pages/beta/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/Beta.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>👀 So you found us...</h1>
+    </main>
+  );
+}
